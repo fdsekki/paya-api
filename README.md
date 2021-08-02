d1a18cff5345dc7c360e08f099f728159a3a737a
Alexandra <dfranciuc@mail.ru>
2021-08-02 23:26:05 +0300

    fixed the received responses from the mutations, added a query to view all users

--- README.md
+++ README.md
@@ -24,19 +24,19 @@ API made with graphql-mysql-nodejs with endpoints like register and make a trans
    f) recipient - datatype VARCHAR(254), NN
    g) sender - datatype VARCHAR(254), NN
 
-4. Go to `https://laboratory.stellar.org/#?network=test` and create two account, take the public key and secret key from there and add them to mysql table users, public key is stellarAccount and secret key is stellarSeed, one account should be maib with email maib@maib.com and another account should be paya with email paya@paya.com. Don't forget to fund these account with friendbot so they will become valid.
-5. Go to `createIssuer.js` and where you see comment `// paya public account` change the value with the public key that you generated for paya@paya.com account from the database.
+4. Go to `https://laboratory.stellar.org/#?network=test` and create two account, take the public key and secret key from there and add them to mysql table users, public key is stellarAccount and secret key is stellarSeed, one account should be maib with email maib@maib.com and another account should be paya with email paya@paya.com. Don't forget to fund these accounts with friendbot so they will become valid.
+5. Go to `createIssuer.js` and where you see commented `// paya public account` change the value with the public key that you generated for paya@paya.com account from the database.
 6. Go to `utils.js` and where you see comments like `// maib public key`, `// paya public key`, `// paya secret key` change the values with the new ones generated in the stellar laboratory and added to the database.
-7. Go to `resolvers.js` and where you see comment `// maib public key` change the value with the new one generated in the stellar laboratory and added to the database.
+7. Go to `resolvers.js` and where you see commented `// maib public key` change the value with the new one generated in the stellar laboratory and added to the database.
 8. Within the terminal go to the api folder and type `yarn` to install all the packages
 9. After installation is done type `node createIssuer.js`, here we are sending to paya account 1000 MDLT from issuer, you should see console log with `All set` if everything worked as supposed, also you can check payments for the account in stellar laboratory.
-10. Type `yarn start`
+10. Type `yarn run dev`
 11. From the browser go to `localhost:9000/graphiql` and create two users:
     `mutation { createUser(email: "example@example.com", name: "example", surname: "example", password: "example"){ email } }`
     `mutation { createUser(email: "example1@example.com", name: "example", surname: "example", password: "example"){ email } }`
-    To check it they were created successfully go to the MySQL Workbench in tab Query 1 and type ther `use paya; select * from users;` and you should see two new rows that we created.
+    To check it they were created successfully go to the MySQL Workbench in tab Query 1 and type there `use paya; select * from users;` and you should see two new rows that we created.
 
-When we created two new users, they're accounts were funded with 10000 XLM and 50 MDLT from the paya account. You can check it in the stellar laboratory at explore endpoints and payments by adding there the public key of one of the account.
+When we created two new users, they're accounts were funded with 10000 XLM and 50 MDLT from the paya account. You can check it in the stellar laboratory at explore endpoints and payments by adding there the public key of one of the accounts.
 
 12. Now that we have two account, we can try to send a payment from one user to another, in the `localhost:9000/graphiql` type:
     `mutation { createPayment (currency_receiving: "MDLT", currency_sending:"MDLT", quantity: 5, recipient: "example@example.com", sender: "example1@example.com", memo:"test transaction") }`
--- resolvers.js
+++ resolvers.js
@@ -1,10 +1,11 @@
 const Stellar = require("stellar-sdk");
 // const db = require("./db");
 const fetch = require("node-fetch");
-const { createTrustline, allowTrustline, payment } = require("./utils");
+const { createTrustline, allowTrustline } = require("./utils");
 // const CryptoJS = require("crypto-js");
 const stellarServer = new Stellar.Server("https://horizon-testnet.stellar.org");
 require("dotenv").config();
+const mysqlPromise = require("mysql2/promise");
 const mysql = require("mysql2");
 // const { v4: uuidv4 } = require("uuid");
 const { TimeoutInfinite } = require("stellar-base");
@@ -20,16 +21,34 @@ const Query = {
   userById: (root, args, context, info) => {
     return "Hello World";
   },
+
+  users: async (root, args, context, info) => {
+    const connection = await mysqlPromise.createConnection(
+      process.env.DATABASE_URL
+    );
+
+    const [rows, fields] = await connection.execute("SELECT * FROM users", [
+      true,
+    ]);
+
+    for (let i = 0; i < rows.length; i++) {
+      return rows;
+    }
+
+    await connection.end();
+  },
 };
 
 const Mutation = {
   createUser: async (root, args, context, info) => {
-    /**
-     * ! things to do for security:
-     * ? encrypt the seeds when adding to db
-     * ? make sure to be protected from sql injection with mysql.escape()
-     * ? never use plain text keys in code
-     * ? all functions that can be in utils.js file must be rearranged from resolvers.js
+    /*
+     *
+     * things to do for security:
+     * encrypt the seeds when adding to db
+     * make sure to be protected from sql injection with mysql.escape()
+     * never use plain text keys in code
+     * all functions that can be in utils.js file must be rearranged from resolvers.js
+     *
      */
 
     const keypair = Stellar.Keypair.random();
@@ -40,7 +59,11 @@ const Mutation = {
       mysql.escape(args.surname),
       mysql.escape(keypair.publicKey()),
       mysql.escape(keypair.secret()),
-      // mysql.escape(await encryptedSecret.toString("utf-8")),
+      /*
+       *
+       * mysql.escape(await encryptedSecret.toString("utf-8")),
+       *
+       */
     ];
 
     const fundAccounts = async () => {
@@ -55,116 +78,108 @@ const Mutation = {
       }
     };
 
-    /* 
+    /*
+     *
+     * to decrypt:
+     *
+     * utils
+     *   .encrypt(Buffer.from("abc", "utf-8"))
+     *   .then(utils.decrypt)
+     *   .then((plaintext) => {
+     *     console.log(plaintext.toString("utf-8"));
+     *   });
+     *
+     *
+     *
+     * let encryptedSecret = await utils.encrypt(
+     *   Buffer.from(keypair.secret(), "utf-8")
+     * );
+     *
+     */
 
-      to decrypt:
-      
-      utils
-        .encrypt(Buffer.from("abc", "utf-8"))
-        .then(utils.decrypt)
-        .then((plaintext) => {
-          console.log(plaintext.toString("utf-8"));
+    const userCreation = async () => {
+      await fundAccounts()
+        .then(async () => {
+          console.log("\n***FUNDING_REPONSE: Account funded succesfully.");
+          await createTrustline(keypair.publicKey(), keypair.secret()).then(
+            async () => {
+              con.query(sql, new_user, (err, results, fields) => {
+                if (err) {
+                  return console.error("\n***DATABASE_ERROR: ", err.message);
+                }
+                console.log(
+                  "\n***DATABASE_RESPONSE: New user added to the database."
+                );
+              });
+
+              await allowTrustline(con, keypair.publicKey());
+            }
+          );
+        })
+        .catch((e) => {
+          console.log("\n***FUNDING_ERROR: ", e);
+          throw e;
         });
+    };
 
-    */
+    const getResponse = async () => {
+      const connection = await mysqlPromise.createConnection(
+        process.env.DATABASE_URL
+      );
 
-    // let encryptedSecret = await utils.encrypt(
-    //   Buffer.from(keypair.secret(), "utf-8")
-    // );
+      const [rows, fields] = await connection.execute(
+        "SELECT * FROM users ORDER BY idusers DESC LIMIT 1",
+        [true]
+      );
 
-    await fundAccounts()
-      .then(() => {
-        console.log("\n***FUNDING_REPONSE: Account funded succesfully.");
-        createTrustline(keypair.publicKey(), keypair.secret()).then(
-          async () => {
-            con.query(sql, new_user, (err, results, fields) => {
-              if (err) {
-                return console.error("\n***DATABASE_ERROR: ", err.message);
-              }
-              console.log(
-                "\n***DATABASE_RESPONSE: New user added to the database."
-              );
-            });
+      for (let i = 0; i < rows.length; i++) {
+        return rows[i];
+      }
 
-            await allowTrustline(con, keypair.publicKey());
-          }
-        );
-      })
-      .catch((e) => {
-        console.log("\n***FUNDING_ERROR: ", e);
-        throw e;
-      });
+      await connection.end();
+    };
+
+    function sleep(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    await userCreation();
+    await sleep(8000);
+    return await getResponse();
   },
 
   createPayment: async (root, args, context, info) => {
-    /**
-     * ! things to do for security
-     * ? decrypt the seeds when querying data from db
-     * ? make sure to be protected from sql injection with mysql.escape()
-     * ? never use plain text keys in code
-     * ? all functions that can be in utils.js file must be rearranged from resolvers.js
+    /*
+     *
+     * things to do for security
+     * decrypt the seeds when querying data from db
+     * make sure to be protected from sql injection with mysql.escape()
+     * never use plain text keys in code
+     * all functions that can be in utils.js file must be rearranged from resolvers.js
+     *
      */
 
-    let senderSql =
-      `SELECT * FROM users WHERE email LIKE ` + "'%" + args.sender + "%' ";
-    let recipientSql =
-      `SELECT * FROM users WHERE email LIKE ` + "'%" + args.recipient + "%' ";
+    const paymentCreation = async () => {
+      let senderSql =
+        `SELECT * FROM users WHERE email LIKE ` + "'%" + args.sender + "%' ";
+      let recipientSql =
+        `SELECT * FROM users WHERE email LIKE ` + "'%" + args.recipient + "%' ";
 
-    con.query(senderSql, [true], async (error, results, fields) => {
-      if (error) {
-        return console.log(error.message);
-      }
-
-      const senderAccount = results.map((usr) => {
-        const replacedEmail = usr.email.replace("'", "");
-        const replacedStellarAccount = usr.stellarAccount.replace("'", "");
-
-        if (replacedEmail.replace("'", "") === args.sender) {
-          return replacedStellarAccount.replace("'", "");
-        }
-      });
-
-      const responseSender = await Promise.all(senderAccount).then(
-        (response) => {
-          for (let i = 0; i <= response.length; i++) {
-            if (typeof response[i] !== "undefined") {
-              return response[i];
-            }
-          }
-        }
-      );
-
-      const senderSeed = results.map((usr) => {
-        const replacedEmail = usr.email.replace("'", "");
-        const replacedStellarSeed = usr.stellarSeed.replace("'", "");
-        if (replacedEmail.replace("'", "") === args.sender) {
-          return replacedStellarSeed.replace("'", "");
-        }
-      });
-
-      const responseSenderSeed = await Promise.all(senderSeed).then(
-        (response) => {
-          for (let i = 0; i <= response.length; i++) {
-            return response[i];
-          }
-        }
-      );
-
-      con.query(recipientSql, [true], async (error, results, fields) => {
+      con.query(senderSql, [true], async (error, results, fields) => {
         if (error) {
-          return console.log(error.message);
+          return console.log("***SENDERSQL_ERROR: ", error.message);
         }
 
-        const recipientAccount = results.map((usr) => {
+        const senderAccount = results.map((usr) => {
           const replacedEmail = usr.email.replace("'", "");
           const replacedStellarAccount = usr.stellarAccount.replace("'", "");
 
-          if (replacedEmail.replace("'", "") === args.recipient) {
+          if (replacedEmail.replace("'", "") === args.sender) {
             return replacedStellarAccount.replace("'", "");
           }
         });
 
-        const responseRecipient = await Promise.all(recipientAccount).then(
+        const responseSender = await Promise.all(senderAccount).then(
           (response) => {
             for (let i = 0; i <= response.length; i++) {
               if (typeof response[i] !== "undefined") {
@@ -174,69 +189,140 @@ const Mutation = {
           }
         );
 
-        const account = await stellarServer.loadAccount(responseSender);
-        const AnchorMDLT = new Stellar.Asset(
-          "MDLT",
-          "GCEFEX2OMYPOCPYJZDUWLHHY4ESOST7JYQ5TOGDFVKRRIQCNNGPOMKK7" // maib(issuer) public key
+        const senderSeed = results.map((usr) => {
+          const replacedEmail = usr.email.replace("'", "");
+          const replacedStellarSeed = usr.stellarSeed.replace("'", "");
+          if (replacedEmail.replace("'", "") === args.sender) {
+            return replacedStellarSeed.replace("'", "");
+          }
+        });
+
+        const responseSenderSeed = await Promise.all(senderSeed).then(
+          (response) => {
+            for (let i = 0; i <= response.length; i++) {
+              return response[i];
+            }
+          }
         );
-        const asset =
-          args.currency_receiving === "XLM"
-            ? Stellar.Asset.native()
-            : AnchorMDLT;
-        const amount = args.quantity;
-        const standardFee = await stellarServer.fetchBaseFee();
-        const senderSeed = Stellar.Keypair.fromSecret(responseSenderSeed);
-        const memo = args.memo;
-        const transactionOptions = {
-          fee: standardFee,
-          networkPassphrase: Stellar.Networks.TESTNET,
-        };
-        const payment = {
-          destination: responseRecipient,
-          asset: asset,
-          amount: amount.toString(),
-        };
 
-        const transaction = new Stellar.TransactionBuilder(
-          account,
-          transactionOptions
-        )
-          .addOperation(Stellar.Operation.payment(payment))
-          .addMemo(Stellar.Memo.text(memo))
-          .setTimeout(TimeoutInfinite)
-          .build();
+        con.query(recipientSql, [true], async (error, results, fields) => {
+          if (error) {
+            return console.log("***RECIPIENTSQL_ERROR: ", error.message);
+          }
+          const recipientAccount = results.map((usr) => {
+            const replacedEmail = usr.email.replace("'", "");
+            const replacedStellarAccount = usr.stellarAccount.replace("'", "");
 
-        transaction.sign(senderSeed);
-
-        try {
-          await stellarServer.submitTransaction(transaction).then(() => {
-            console.log("OK");
-
-            let sql = `INSERT INTO transactions(currency_receiving, currency_sending, quantity, send_date, recipient, sender) VALUES(?, ?, ?, ?, ?, ?)`;
-            let new_transaction = [
-              mysql.escape(args.currency_receiving),
-              mysql.escape(args.currency_sending),
-              mysql.escape(args.quantity),
-              new Date(),
-              mysql.escape(args.recipient),
-              mysql.escape(args.sender),
-            ];
-
-            con.query(sql, new_transaction, (err, results, fields) => {
-              if (err) {
-                return console.error(err.message);
-              }
-
-              console.log("new transaction made");
-            });
+            if (replacedEmail.replace("'", "") === args.recipient) {
+              return replacedStellarAccount.replace("'", "");
+            }
           });
-        } catch (e) {
-          console.log(`failure ${e}`);
 
-          throw e;
-        }
+          const responseRecipient = await Promise.all(recipientAccount).then(
+            (response) => {
+              for (let i = 0; i <= response.length; i++) {
+                if (typeof response[i] !== "undefined") {
+                  return response[i];
+                } else {
+                }
+              }
+            }
+          );
+
+          const account = await stellarServer.loadAccount(responseSender);
+          const AnchorMDLT = new Stellar.Asset(
+            "MDLT",
+            "GCEFEX2OMYPOCPYJZDUWLHHY4ESOST7JYQ5TOGDFVKRRIQCNNGPOMKK7" // maib(issuer) public key
+          );
+          const asset =
+            args.currency_receiving === "XLM"
+              ? Stellar.Asset.native()
+              : AnchorMDLT;
+          const amount = args.quantity;
+          const standardFee = await stellarServer.fetchBaseFee();
+          const senderSeed = Stellar.Keypair.fromSecret(responseSenderSeed);
+          const memo = args.memo;
+          const transactionOptions = {
+            fee: standardFee,
+            networkPassphrase: Stellar.Networks.TESTNET,
+          };
+          const payment = {
+            destination: responseRecipient,
+            asset: asset,
+            amount: amount.toString(),
+          };
+
+          const transaction = new Stellar.TransactionBuilder(
+            account,
+            transactionOptions
+          )
+            .addOperation(Stellar.Operation.payment(payment))
+            .addMemo(Stellar.Memo.text(memo))
+            .setTimeout(TimeoutInfinite)
+            .build();
+
+          transaction.sign(senderSeed);
+
+          try {
+            await stellarServer.submitTransaction(transaction).then(() => {
+              console.log(
+                "***SUBMIT_TRANSACTION_RESPONSE: Transaction submitted successfully."
+              );
+
+              let sql = `INSERT INTO transactions(currency_receiving, currency_sending, quantity, send_date, recipient, sender, memo) VALUES(?, ?, ?, ?, ?, ?, ?)`;
+              let new_transaction = [
+                mysql.escape(args.currency_receiving),
+                mysql.escape(args.currency_sending),
+                mysql.escape(args.quantity),
+                new Date(),
+                mysql.escape(args.recipient),
+                mysql.escape(args.sender),
+                mysql.escape(args.memo),
+              ];
+
+              con.query(sql, new_transaction, (err, results, fields) => {
+                if (err) {
+                  return console.error(err.message);
+                }
+
+                console.log(
+                  "***DATABASE_RESPONSE: Transaction added successfully."
+                );
+              });
+            });
+          } catch (e) {
+            console.log(`***TRANSACTION_ERROR: ${e}`);
+
+            throw e;
+          }
+        });
       });
-    });
+    };
+
+    const getResponse = async () => {
+      const connection = await mysqlPromise.createConnection(
+        process.env.DATABASE_URL
+      );
+
+      const [rows, fields] = await connection.execute(
+        "SELECT * FROM transactions ORDER BY idtransactions DESC LIMIT 1",
+        [true]
+      );
+
+      for (let i = 0; i < rows.length; i++) {
+        return rows[i];
+      }
+
+      await connection.end();
+    };
+
+    function sleep(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    await paymentCreation();
+    await sleep(8000);
+    return await getResponse();
   },
 };
 
